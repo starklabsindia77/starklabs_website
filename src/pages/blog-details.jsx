11ea136ebee482eeb328397d8b6098e5d35d5b2c
varun singh <pratap@arpiantech.com>
2023-11-12 23:55:55 +0530

    changes

--- src/pages/blog-details.jsx
+++ src/pages/blog-details.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import SEO from "../common/seo";
-import BlogDetails from "../components/blog-details";
+// import BlogDetails from "../components/blog-details";
 import Wrapper from "../layout/wrapper";
 
 const index = () => {
   return (
     <Wrapper>
       <SEO pageTitle={"Starklabs - We power your business with cutting-edge IT solutions"} />
-      <BlogDetails />
+
     </Wrapper>
   );
 };
